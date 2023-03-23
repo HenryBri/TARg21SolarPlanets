c11265f7ed89d881a400f5c432816ea833bb7b9f
Opilane TTHK <opilane@tthk.ee>
2023-03-23 15:55:03 +0200

    Jupiter and Saturn

--- TARge21SolarPlanets/Views/StartPage.xaml.cs
+++ TARge21SolarPlanets/Views/StartPage.xaml.cs
@@ -22,6 +22,10 @@ public partial class StartPage : ContentPage
 
 		parentAnimation.Add(0.2, 0.4, new Animation(v => imgEarth.Opacity = v, 0, 1, Easing.CubicIn));
 
-		parentAnimation.Commit(this, "TransitionAnimation", 16, 3000, null, null);
+        parentAnimation.Add(0.4, 0.6, new Animation(v => imgJupiter.Opacity = v, 0, 1, Easing.CubicIn));
+
+        parentAnimation.Add(0.5, 0.7, new Animation(v => imgSaturn.Opacity = v, 0, 1, Easing.CubicIn));
+
+        parentAnimation.Commit(this, "TransitionAnimation", 16, 3000, null, null);
 	}
 }
