ab23303960fefbf1766c245b2ffeb669d5953ac4
HenryBri <99488927+HenryBri@users.noreply.github.com>
2023-03-30 14:08:58 +0300

    AllPlanetsAdded

--- TARge21SolarPlanets/GlobalUsings.cs
+++ TARge21SolarPlanets/GlobalUsings.cs
@@ -1,1 +1,4 @@
 ﻿global using Views;
+
+
+global using Microsoft.Maui.Controls;
--- TARge21SolarPlanets/Views/StartPage.xaml.cs
+++ TARge21SolarPlanets/Views/StartPage.xaml.cs
@@ -26,6 +26,22 @@ public partial class StartPage : ContentPage
 
         parentAnimation.Add(0.5, 0.7, new Animation(v => imgSaturn.Opacity = v, 0, 1, Easing.CubicIn));
 
+        parentAnimation.Add(0.3, 0.5, new Animation(v => imgMars.Opacity = v, 0, 1, Easing.CubicIn));
+
+        parentAnimation.Add(0.4, 0.6, new Animation(v => imgJupiter.Opacity = v, 0, 1, Easing.CubicIn));
+        
+		parentAnimation.Add(0.5, 0.7, new Animation(v => imgSaturn.Opacity = v, 0, 1, Easing.CubicIn));
+
+        parentAnimation.Add(0.7, 0.9, new Animation(v => imgUranus.Opacity = v, 0, 1, Easing.CubicIn));
+
+        parentAnimation.Add(0.6, 0.8, new Animation(v => imgNeptune.Opacity = v, 0, 1, Easing.CubicIn));
+        
+
         parentAnimation.Commit(this, "TransitionAnimation", 16, 3000, null, null);
-	}
+
+        parentAnimation.Add(0.7, 1, new Animation(v => frmIntro.Opacity = v, 0, 1, Easing.CubicIn));
+    }
+
+    async void ExploreNow_Clicked(System.Object sender, System.EventArgs e)
+        => Application.Current.MainPage = new NavigationPage(new PlanetsPage());
 }
