466a0d2a7ace801c81d66fc369b1f8d178d3eb68
HenryBri <99488927+HenryBri@users.noreply.github.com>
2023-04-06 15:50:00 +0300

    earth

--- TARge21SolarPlanets/TARge21SolarPlanets/Services/PlanetsService.cs
+++ TARge21SolarPlanets/TARge21SolarPlanets/Services/PlanetsService.cs
@@ -7,7 +7,7 @@
             new()
             {
                 Name = "Mercury",
-                Subtitle = "The smallest planet",
+                Subtitle = "  The smallest planet",
                 HeroImage = "mercury.png",
                 Description = "Mercury is the smallest planet in the Solar System and the closest to the Sun. Its orbit around the Sun takes 87.97 Earth days, the shortest of all the Sun's planets. It is named after the Roman god Mercurius (Mercury), god of commerce, messenger of the gods, and mediator between gods and mortals, corresponding to the Greek god Hermes (Ἑρμῆς). Like Venus, Mercury orbits the Sun within Earth's orbit as an inferior planet; its apparent distance from the Sun as viewed from Earth never exceeds 28°.",
                 AccentColorStart = Color.FromArgb("#353535"),
@@ -17,6 +17,38 @@
                     "https://en.wikipedia.org/wiki/Mercury_(planet)#/media/File:Mercury_in_true_color.jpg",
                     "https://en.wikipedia.org/wiki/Mercury_(planet)#/media/File:EW1027346412Gnomap.png",
                     "https://solarsystem.nasa.gov/system/feature_items/images/75_mercury_carousel_1.jpg"
+                }
+            },
+
+            new()
+            {
+                Name = "Venus",
+                Subtitle = "  The pressure cooker",
+                HeroImage = "venus.png",
+                Description = "Venus is the second planet from the Sun. It is sometimes called Earth's sister or twin planet as it is almost as large and has a similar composition. As an interior planet to Earth, Venus (like Mercury) appears in Earth's sky never far from the Sun, either as morning star or evening star. Aside from the Sun and Moon, Venus is the brightest natural object in Earth's sky, capable of casting visible shadows on Earth in dark conditions and being visible to the naked eye in broad daylight.",
+                AccentColorStart = Color.FromArgb("#a6393b"),
+                AccentColorEnd = Color.FromArgb("#d17f21"),
+                Images = new()
+                {
+                    "https://en.wikipedia.org/wiki/Venus#/media/File:Venus_-_December_23_2016.png",
+                    "https://en.wikipedia.org/wiki/Venus#/media/File:Venus_2_Approach_Image.jpg",
+                    "https://en.wikipedia.org/wiki/Venus#/media/File:PIA00084_Eistla_region_pancake_volcanoes.jpg"
+                }
+            },
+
+            new()
+            {
+                Name = "Earth",
+                Subtitle = "  The cradle of life",
+                HeroImage = "earth.png",
+                Description = "Earth is the third planet from the Sun and the only place known in the universe where life has originated and found habitability. While Earth may not contain the largest volumes of water in the Solar System, only Earth sustains liquid surface water, extending over 70.8% of the Earth with its ocean, making Earth an ocean world. Earth's polar regions currently retain most of all other water with large sheets of ice covering ocean and land, dwarfing Earth's groundwater, lakes, rivers and atmospheric water.",
+                AccentColorStart = Color.FromArgb("#0e3d68"),
+                AccentColorEnd = Color.FromArgb("#2e97c7"),
+                Images = new()
+                {
+                    "https://en.wikipedia.org/wiki/Earth#/media/File:The_Blue_Marble_(remastered).jpg",
+                    "https://en.wikipedia.org/wiki/Earth#/media/File:Ocean_world_Earth.jpg",
+                    "https://en.wikipedia.org/wiki/Earth#/media/File:ISS-42_Waning_sun.jpg"
                 }
             }
         };
